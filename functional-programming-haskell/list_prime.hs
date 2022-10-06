31ddd4b3e1d82f5d6a3b1f2e41704a8fb25fbfc9
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-10-06 21:34:01 +0700

    update list_prime.hs

--- functional-programming-haskell/list_prime.hs
+++ functional-programming-haskell/list_prime.hs
@@ -3,7 +3,7 @@ checkPrime 1 = False
 checkPrime 2 = True
 checkPrime n 
     | (length [x | x <- [2 .. n-1], mod n x == 0]) > 0 = False
-	| otherwise = True
+    | otherwise = True
 
 listPrime :: Int -> [Int]
 listPrime n = [x | x <- [2 .. n], checkPrime x]
