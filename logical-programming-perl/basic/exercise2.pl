1fa340d879f423ec575fe7fc0b6a39c258e2dd62
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-11-03 19:42:17 +0700

    update exercise2.pl

--- logical-programming-perl/basic/exercise2.pl
+++ logical-programming-perl/basic/exercise2.pl
@@ -1,6 +1,9 @@
+/* 2. (a) Knowledge base */
 subdir(documents, private).
 subdir(documents, work).
 subdir(private, images).
 subdir(private, videos).
 subdir(work, research).
 subdir(work, teaching).
+
+/* 2. (b) Add rules 'descendants' to retrieve descendant nodes */
