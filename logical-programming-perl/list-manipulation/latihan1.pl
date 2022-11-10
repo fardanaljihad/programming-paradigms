3dad4e38d089cf38884eec40484270c079eb6561
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-11-10 21:54:35 +0700

    add latihan1.pl

---
+++ logical-programming-perl/list-manipulation/latihan1.pl
@@ -0,0 +1,6 @@
+what_is(["mata kuliah", 
+         "Prinsip Bahasa Pemrograman", 
+         "Pemrograman Berorientasi Object", 
+         "Teknik Pemrograman"]).
+
+what_is([Category|MataKuliah]) :- member(Category, MataKuliah).
