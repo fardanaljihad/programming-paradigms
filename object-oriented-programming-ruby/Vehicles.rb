990b538a51817780ec3633f3b4746e8a1a2daa84
fardantif21 <fardan.al.tif421@polban.ac.id>
2022-11-25 15:08:03 +0700

    update Vehicles.rb

--- object-oriented-programming-ruby/Vehicles.rb
+++ object-oriented-programming-ruby/Vehicles.rb
@@ -1,15 +1,35 @@
 class Vehicle
     attr_accessor :year
 
-    def initialize(y)
-        self.year = y
+    def initialize(year)
+        @year = year
     end
 
     def printStats
-        "#{self.year}"
+        puts "#{self.year}"
+    end
+end
+
+class Car < Vehicle
+    attr_accessor :model, :brand
+
+    def initialize(year, model, brand)
+        super year
+        @model = model
+        @brand = brand
+    end
+
+    def printStats
+        super
+        puts "#{@model}"
+        puts "#{@brand}"
     end
 end
 
 mobil = Vehicle.new(1995)
 
 puts mobil.printStats
+
+mobil2 = Car.new(1998, "supercar", "BMW")
+
+puts mobil2.printStats()
